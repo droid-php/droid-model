820c74d6842d55df45bd0be6feb0d5da647a3a44
boite <boite@users.noreply.github.com>
2016-10-18 22:01:16 +0100

    Fix white space.

--- src/Model/Feature/Firewall/Rule.php
+++ src/Model/Feature/Firewall/Rule.php
@@ -65,16 +65,15 @@ class Rule
         $this->action = $action;
         return $this;
     }
-    
+
     public function getComment()
     {
         return $this->comment;
     }
-    
+
     public function setComment($comment)
     {
         $this->comment = $comment;
         return $this;
     }
-    
 }
